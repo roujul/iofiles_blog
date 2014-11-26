1cfc21e1b8ed7879e60e17df29c06a7ea26cc8fb
Julien Roux <julien.roux@EWGECO021.ewgeco.local>
2014-11-26 16:09:46 +0000

    CoveragCoveragee

--- src/main/java/com/code_roux/blog/read_file/ReadingWritingFileJava7.java
+++ src/main/java/com/code_roux/blog/read_file/ReadingWritingFileJava7.java
@@ -1,10 +1,13 @@
 package com.code_roux.blog.read_file;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadingWritingFileJava7 {
@@ -14,9 +17,34 @@ public class ReadingWritingFileJava7 {
 		return Files.readAllLines(path, StandardCharsets.UTF_8);
 	}
 
-	void writeSmallTextFile(List<String> lines, String fileName)
+	public void writeSmallFile(final List<String> lines, final String fileName)
 			throws IOException {
 		Path path = Paths.get(fileName);
 		Files.write(path, lines, StandardCharsets.UTF_8);
+	}
+
+	public List<String> readLargeFile(String fileName) throws IOException {
+		Path path = Paths.get(fileName);
+		List<String> lines = new ArrayList<String>();
+		try (BufferedReader reader = Files.newBufferedReader(path,
+				StandardCharsets.UTF_8)) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
+			}
+		}
+		return lines;
+	}
+
+	public void writeLargeTextFile(String aFileName, List<String> aLines)
+			throws IOException {
+		Path path = Paths.get(aFileName);
+		try (BufferedWriter writer = Files.newBufferedWriter(path,
+				StandardCharsets.UTF_8)) {
+			for (String line : aLines) {
+				writer.write(line);
+				writer.newLine();
+			}
+		}
 	}
 }
--- src/test/java/com/code_roux/blog/read_file/ReadingWritingFileJava7Test.java
+++ src/test/java/com/code_roux/blog/read_file/ReadingWritingFileJava7Test.java
@@ -1,29 +1,77 @@
 package com.code_roux.blog.read_file;
 
 import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.fest.assertions.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReadingWritingFileJava7Test {
 	
-	@Test
-	public void testApp() throws Exception {
-		File temp = File.createTempFile("tempfile", ".tmp"); 
+	private ReadingWritingFileJava7 io;
+	private File temp;
+	
+	@Before
+	public void setup() throws Exception {
+		io = new ReadingWritingFileJava7();
+		temp = File.createTempFile("tempfile", ".tmp"); 
 		Assertions.assertThat(temp).exists();
-
-		ReadingWritingFileJava7 io = new ReadingWritingFileJava7();
-		List<String> list = io.readSmallFile(temp.getAbsolutePath());
-		Assertions.assertThat(list).isEmpty();
-
+	}
+	
+	@Test
+	public void testReadingSmallFile() throws Exception {
+		URL resource = getClass().getResource("test.txt");
+		Assertions.assertThat(resource).isNotNull();
+		String path = Paths.get(resource.toURI()).toString();
+		List<String> list = io.readSmallFile(path);
+		Assertions.assertThat(list).isNotEmpty();
+	}
+	
+	@Test
+	public void testWritingSmallFile() throws Exception {
+		List<String> list = new ArrayList<String>();
 		list.add("run 1");
-		io.writeSmallTextFile(list, temp.getAbsolutePath());
+		io.writeSmallFile(list, temp.getAbsolutePath());
 
 		list.add("run 2");
-		io.writeSmallTextFile(list, temp.getAbsolutePath());
+		io.writeSmallFile(list, temp.getAbsolutePath());
 		
 		list = io.readSmallFile(temp.getAbsolutePath());
 		Assertions.assertThat(list).hasSize(2);
+	}
+
+	@Test
+	public void testLoadWritingLargeFile() throws Exception {
+		File temp = File.createTempFile("tempfile", ".tmp"); 
+		Assertions.assertThat(temp).exists();
+
+		List<String> list = new ArrayList<String>();
+		for(int i = 0; i < 5000000; i++) {
+			list.add("1324567890132456789013245678901324567890132456789013245678901324567890132456789013245678901324567890" + i);
+		}
+		
+		io.writeSmallFile(list, temp.getAbsolutePath());
+		List<String> read = io.readLargeFile(temp.getAbsolutePath());
+		Assertions.assertThat(read).hasSize(5000000);
+	}
+	
+
+	@Test
+	public void testReadingWritingLargeFile() throws Exception {
+		File temp = File.createTempFile("tempfile", ".tmp"); 
+		Assertions.assertThat(temp).exists();
+
+		List<String> list = new ArrayList<String>();
+		for(int i = 0; i < 5000000; i++) {
+			list.add("1324567890132456789013245678901324567890132456789013245678901324567890132456789013245678901324567890" + i);
+		}
+		
+		io.writeLargeTextFile(temp.getAbsolutePath(), list);
+		List<String> read = io.readLargeFile(temp.getAbsolutePath());
+		Assertions.assertThat(read).hasSize(5000000);
 	}
 }
